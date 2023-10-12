442a64087c92c680272c2f25e85c33eb21a41547
Stuart Thomson <me@sthom.kiwi>
2023-10-12 15:05:59 +1300

    Cache animation file loading

--- src/components/react/StickerBook/stickers/LottieSticker.tsx
+++ src/components/react/StickerBook/stickers/LottieSticker.tsx
@@ -2,6 +2,22 @@ import lottie, { type AnimationItem } from "lottie-web";
 import { useEffect, useRef, useState } from "react";
 import type { LottieStickerData, StickerInfo } from "../types";
 
+async function requestLottieFile(url: string) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+const LOTTIE_PROMISE_CACHE: { [key: string]: any } = {};
+
+async function getLottieData(url: string) {
+  if (!LOTTIE_PROMISE_CACHE[url]) {
+    LOTTIE_PROMISE_CACHE[url] = requestLottieFile(url);
+  }
+
+  return LOTTIE_PROMISE_CACHE[url]!;
+}
+
 export interface LottieStickerProps {
   sticker: StickerInfo;
   data: LottieStickerData;
@@ -12,21 +28,37 @@ export function LottieSticker({ data }: LottieStickerProps) {
   const [, setAnimation] = useState<AnimationItem>();
 
   useEffect(() => {
-    if (!ref.current) {
-      return () => {};
+    let anim: AnimationItem | undefined;
+    let didCleanup = false;
+
+    async function go() {
+      if (!ref.current) {
+        return;
+      }
+
+      const animationData = await getLottieData(data.url);
+      // This is a guard against strict mode's double effect behaviour
+      if (didCleanup) {
+        return;
+      }
+
+      anim = lottie.loadAnimation({
+        container: ref.current,
+        animationData,
+        loop: true,
+        autoplay: true,
+        name: data.name,
+      });
+      setAnimation(anim);
     }
 
-    const anim = lottie.loadAnimation({
-      container: ref.current,
-      path: data.url,
-      loop: true,
-      autoplay: true,
-      name: data.name,
-    });
-    setAnimation(anim);
+    go();
 
     return () => {
-      anim.destroy();
+      didCleanup = true;
+      if (anim) {
+        anim.destroy();
+      }
     };
   }, []);
 
