d7caa10cfdbf84e9cce76c9d22f75a0f10fef81b
Stuart Thomson <me@sthom.kiwi>
2023-12-30 17:49:01 +1300

    Move blog page to normal page
    For some reason, this fixes an issue where some CSS wasn't being included. I have no idea why...

--- src/lib/notion/overrides.ts
+++ src/lib/notion/overrides.ts
@@ -1,4 +1,5 @@
 import type { NotionOverrideableProps } from "../../components/notion/blocks/types";
+import BlogCollection from "../../components/notion/overrides/blog/BlogCollection.astro";
 import ProjectsCollection from "../../components/notion/overrides/projects/ProjectsCollection.astro";
 
 export async function getComponentOverrides(
@@ -8,6 +9,11 @@ export async function getComponentOverrides(
     case "f745c582-4881-4b70-badf-70b005f1bd72":
       return {
         child_database: ProjectsCollection,
+      };
+    case "b749d283-ebbd-4b6f-92d4-13301479c2da": // Both blog pages end up matching here
+    case "13389202-34aa-41c3-8bdd-dcda10f24840":
+      return {
+        child_database: BlogCollection,
       };
     default:
       return {};
