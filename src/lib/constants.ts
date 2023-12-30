b5df7fea0d79729833ca22b60c84ffb9333d4499
Stuart Thomson <me@sthom.kiwi>
2023-12-31 08:49:21 +1300

    Add content to index page
    No styles yet

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,6 +3,8 @@ export const SPACE_ID = "e0a1d437-085c-4280-8d31-2918df11590b";
 export const ROOT_PAGE_ID = "25c54908-c86d-426c-a1c7-af314e09d74b";
 export const BLOG_COLLECTION_ID = "2592db33-2b6e-47e0-925f-5eb313c7520e";
 export const PAGES_COLLECTION_ID = "4e043a71-8fe5-48a1-b460-ed69366d567d";
+export const ACTIVE_PROJECTS_COLLECTION_ID =
+  "d04e2d18-e9d5-4bd9-b398-a0edbe844ee5";
 
 // TODO: not have to use this
 export const BLOG_PAGE_ID = "b749d283-ebbd-4b6f-92d4-13301479c2da";
