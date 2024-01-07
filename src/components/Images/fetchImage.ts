38c8548cec5b0157abe21650ecd0f10bf7e2664b
Stuart Thomson <me@sthom.kiwi>
2024-01-07 18:34:38 +1300

    Modify local image loading because Astro imports didn't work
    It turns out it's a lot more effort to completely ignore  and replace a major feature of the framework you're using.

--- src/components/Images/fetchImage.ts
+++ src/components/Images/fetchImage.ts
@@ -1,3 +1,6 @@
+import { lookup } from "mime-types";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 import { NOISY_LOGS } from "../../lib/constants";
 
 export interface ImageInfo {
@@ -5,27 +8,59 @@ export interface ImageInfo {
   mimeType: string;
 }
 
-const IMAGE_REQUEST_CACHE: Record<string, Promise<ImageInfo>> = {};
+const IMAGE_REQUEST_CACHE = new Map<string, Promise<ImageInfo>>();
 
 export function fetchImage(src: string): Promise<ImageInfo> {
-  if (!IMAGE_REQUEST_CACHE[src]) {
+  if (!IMAGE_REQUEST_CACHE.has(src)) {
     if (NOISY_LOGS) {
       // eslint-disable-next-line no-console
       console.log(`[Images] Fetching from ${src}`);
     }
 
-    IMAGE_REQUEST_CACHE[src] = fetch(src).then(async (response) => {
-      const mimeType = response.headers.get("Content-Type");
-      if (!mimeType) {
-        throw new Error(`Image source ${src} did not have a content type`);
-      }
-      const buffer = await response.arrayBuffer();
-      return {
-        buffer,
-        mimeType,
-      };
-    });
+    const promise = fetch(src).then(
+      async (response) => {
+        const mimeType = response.headers.get("Content-Type");
+        if (!mimeType) {
+          throw new Error(`Image source ${src} did not have a content type`);
+        }
+        const buffer = await response.arrayBuffer();
+        return {
+          buffer,
+          mimeType,
+        };
+      },
+      (err) => {
+        // eslint-disable-next-line no-console
+        console.error(`Error while fetching image at ${src}`);
+        throw err;
+      },
+    );
+    IMAGE_REQUEST_CACHE.set(src, promise);
   }
 
-  return IMAGE_REQUEST_CACHE[src];
+  return IMAGE_REQUEST_CACHE.get(src)!;
+}
+
+export const RESOURCES_DIR = join(process.cwd(), "src/resources");
+
+export function readImage(name: string): Promise<ImageInfo> {
+  if (!IMAGE_REQUEST_CACHE.has(name)) {
+    if (NOISY_LOGS) {
+      // eslint-disable-next-line no-console
+      console.log(`[Images] Reading from ${name}`);
+    }
+
+    const mimeType = lookup(name);
+    if (!mimeType) {
+      throw new Error(`Unknown MIME type for file ${name}`);
+    }
+
+    const promise = readFile(join(RESOURCES_DIR, name)).then((buffer) => ({
+      buffer,
+      mimeType,
+    }));
+    IMAGE_REQUEST_CACHE.set(name, promise);
+  }
+
+  return IMAGE_REQUEST_CACHE.get(name)!;
 }
