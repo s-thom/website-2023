b8ca7b96cefc90508d76d44ecada9e8571215782
Stuart Thomson <me@sthom.kiwi>
2024-09-12 20:24:53 +1200

    Skip syncing pages that don't need syncing

--- src/integrations/notion-loader/loader.ts
+++ src/integrations/notion-loader/loader.ts
@@ -1,10 +1,20 @@
-import { Client, iteratePaginatedAPI } from "@notionhq/client";
-import type { QueryDatabaseParameters } from "@notionhq/client/build/src/api-endpoints";
+import {
+  Client,
+  isFullDatabase,
+  isFullPage,
+  iteratePaginatedAPI,
+} from "@notionhq/client";
+import type {
+  PageObjectResponse,
+  QueryDatabaseParameters,
+} from "@notionhq/client/build/src/api-endpoints";
 import type { Loader } from "astro/loaders";
 import { z } from "astro/zod";
 import PQueue from "p-queue";
 import { collectPageInfo } from "./api";
 import { notionLoaderSchema, propertySchemas } from "./schema";
+
+const INTERNAL_LOADER_VERSION = "1";
 
 export interface NotionLoaderOptions {
   token: string;
@@ -18,7 +28,39 @@ export function notionLoader(options: NotionLoaderOptions): Loader {
 
   return {
     name: "sthom-notion-loader",
-    load: async ({ store, logger, generateDigest, parseData }) => {
+    load: async ({
+      collection,
+      store,
+      logger,
+      meta,
+      generateDigest,
+      parseData,
+    }) => {
+      const lastLoaderVersion = meta.get("version");
+      let forceRefresh = false;
+      if (lastLoaderVersion !== INTERNAL_LOADER_VERSION) {
+        logger.info(`Collection ${collection} needs full sync`);
+        meta.set("version", INTERNAL_LOADER_VERSION);
+        forceRefresh = true;
+      }
+
+      const database = await client.databases.retrieve({
+        database_id: options.databaseId,
+      });
+      if (!isFullDatabase(database)) {
+        throw new Error(
+          `Database ${options.databaseId} for collection ${collection} is not fully accessible by this integration`,
+        );
+      }
+
+      const lastModified = meta.get("last-modified");
+      if (!forceRefresh && database.last_edited_time === lastModified) {
+        logger.info(
+          `Collection ${collection} has not been edited since last update, skipping`,
+        );
+        return;
+      }
+
       const pagesIterable = iteratePaginatedAPI(client.databases.query, {
         database_id: options.databaseId,
         filter: options.filter,
@@ -26,44 +68,81 @@ export function notionLoader(options: NotionLoaderOptions): Loader {
       });
 
       // Pages that get found will be removed from this set, leaving only deleted ones by the end.
-      const previousPageIds = new Set(store.keys());
+      const removePageIds = new Set(store.keys());
 
       const pagesQueue = new PQueue();
 
       // Need to keep track of the order we encounter pages. Since the requesting process
       // is async and done in parallel, things can get out of order.
       const orderedIds: string[] = [];
+      const unchangedPages: string[] = [];
       const dataMap = new Map<string, Parameters<typeof store.set>[0]>();
       for await (const page of pagesIterable) {
+        if (!isFullPage(page)) {
+          throw new Error(
+            `Page ${page.id} (in database ${options.databaseId}) is not fully accessible by this integration`,
+          );
+        }
+
+        removePageIds.delete(page.id);
         orderedIds.push(page.id);
-        pagesQueue.add(async () => {
-          const rawInfo = await collectPageInfo(page.id, logger);
-          previousPageIds.delete(page.id);
 
-          const data = await parseData({
-            id: page.id,
-            data: rawInfo as any,
-          });
+        // Get the previously stored page to see if it needs updating
+        const previousPageData = store.get(page.id);
+        const previousPage = previousPageData
+          ? previousPageData.data.page
+          : undefined;
 
-          dataMap.set(page.id, {
-            id: page.id,
-            digest: generateDigest({
-              lastEdited: rawInfo.page.last_edited_time,
-            }),
-            data,
+        if (
+          !forceRefresh &&
+          previousPageData !== undefined &&
+          typeof previousPage === "object" &&
+          previousPage !== null &&
+          (previousPage as PageObjectResponse).last_edited_time ===
+            page.last_edited_time
+        ) {
+          // No changes necessary. Sill add to data map to ensure order is correct
+          unchangedPages.push(page.id);
+          dataMap.set(page.id, previousPageData);
+        } else {
+          // Need to queue the page update
+          pagesQueue.add(async () => {
+            const rawInfo = await collectPageInfo(page.id, logger);
+
+            const data = await parseData({
+              id: page.id,
+              data: rawInfo as any,
+            });
+
+            dataMap.set(page.id, {
+              id: page.id,
+              digest: generateDigest({
+                lastEdited: rawInfo.page.last_edited_time,
+              }),
+              data,
+            });
           });
-        });
+        }
       }
 
       await pagesQueue.onIdle();
+
+      if (unchangedPages.length > 0) {
+        logger.info(
+          `Pages not modified since last update: ${unchangedPages.join(", ")}`,
+        );
+      }
 
       for (const id of orderedIds) {
         store.set(dataMap.get(id)!);
       }
 
-      for (const id of previousPageIds) {
+      for (const id of removePageIds) {
         store.delete(id);
       }
+
+      meta.set("last-modified", database.last_edited_time);
+      logger.info(`Collection ${collection} done!`);
     },
     schema: async () => {
       const database = await client.databases.retrieve({
