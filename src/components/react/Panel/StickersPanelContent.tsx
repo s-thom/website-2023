d60ff6c0b376c1baa1a48d1c53ea0d85186d9311
Stuart Thomson <me@sthom.kiwi>
2024-09-14 20:49:51 +1200

    Add empty state for sticker panel

--- src/components/react/Panel/StickersPanelContent.tsx
+++ src/components/react/Panel/StickersPanelContent.tsx
@@ -41,18 +41,22 @@ function StickerList({ stickers }: StickerListProps) {
 
   return (
     <ul className="sticker-list">
-      {groupedStickers.map((group) => (
-        <li key={group[0].type} className="sticker-list-sticker-item">
-          <StickerWrapper
-            type={group[0].type}
-            draggable
-            stickerId={group[0].id}
-          />
-          {group.length > 1 && (
-            <span className="sticker-list-count">{group.length}</span>
-          )}
-        </li>
-      ))}
+      {groupedStickers.length > 0 ? (
+        groupedStickers.map((group) => (
+          <li key={group[0].type} className="sticker-list-sticker-item">
+            <StickerWrapper
+              type={group[0].type}
+              draggable
+              stickerId={group[0].id}
+            />
+            {group.length > 1 && (
+              <span className="sticker-list-count">{group.length}</span>
+            )}
+          </li>
+        ))
+      ) : (
+        <div className="sticker-list-empty">No stickers unlocked yet...</div>
+      )}
     </ul>
   );
 }
